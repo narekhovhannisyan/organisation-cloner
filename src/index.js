3967c82796c70f7ab6d55a5adac74d4c85bc0647
narekhovhannisyan <narekgilmour@gmail.com>
2020-04-29 12:46:05 +0400

    src: make module.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,21 @@ const { exec } = require('child_process')
 const Bluebird = require('bluebird')
 const requestPromise = require('request-promise')
 
-const ORGANIZATION = '<YOUR_ORGANIZATION>'
-const TOKEN = '<YOUR_TOKEN>'
+/**
+ * Gets organization repositories using github API.
+ * Then gets `ssh_url` from the repository and clones using ssh.
+ * @param {String} organization
+ * @param {String} token
+ * @returns {Promise.<any>}
+ */
+const cloneOrganizationRepositories = (organization, token) => {
+  const gitHubPath = `http://api.github.com/orgs/${organization}/repos?access_token=${token}`
 
-const gitString = `http://api.github.com/orgs/${ORGANIZATION}/repos?access_token=${TOKEN}`
- 
-requestPromise(gitString, { headers: { 'User-Agent': 'Awesome-Octocat-App' }, json: true })
-  .then((repositories) => repositories.map((repository) => repository.ssh_url))
-  .then((repos) => Bluebird.map(repos, (repo) => exec(`git clone ${repo}`)))
+  return requestPromise(gitHubPath, { headers: { 'User-Agent': 'Awesome-Octocat-App' }, json: true })
+    .then((repositories) => repositories.map((repository) => repository.ssh_url))
+    .then((repos) => Bluebird.map(repos, (repo) => exec(`git clone ${repo}`)))
+}
+
+module.exports = {
+  cloneOrganizationRepositories
+}
